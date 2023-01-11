369e6d1f31119ffd2e2867fcc201a680ecd8b9eb
Jeonghwan <110805149+skysky44@users.noreply.github.com>
2023-01-11 15:15:58 +0900

    [D1] Title: 1대1 가위바위보, Time: 98 ms, Memory: 43,232 KB -BaekjoonHub

---
+++ SWEA/D1/1936. 1대1 가위바위보/1대1 가위바위보.py
@@ -0,0 +1,14 @@
+x, y = map(int, input().split())
+
+if x == 1 and y == 3:
+    print('A')
+elif x == 1 and y == 2:
+    print('B')
+elif x == 2 and y == 3:
+    print('B')
+elif x == 2 and y == 1:
+    print('A')
+elif x == 3 and y == 1:
+    print('B')
+elif x == 3 and y == 2:
+    print('A')
