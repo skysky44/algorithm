47f76b5fb086d0807504efcf4221597933301476
Jeonghwan <110805149+skysky44@users.noreply.github.com>
2023-01-16 15:13:42 +0900

    [Bronze IV] Title: 주사위 세개, Time: 32 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.py
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.py
@@ -1,13 +1,10 @@
 a, b, c = map(int, input().split())
-money = 0
 if a == b == c:
-    money = 10000+a*1000
-elif a == b != c:
-    money = 1000+a*100
-elif a == c != b:
-    money = 1000+a*100
-elif b == c != a:
-    money = 1000+b*100
-elif a != b != c:
-    money = max(a, b, c)*100
-print(money)
+    reward = 10000 + a*1000
+elif a != b and a != c and b != c: # 여기로 
+    reward = max(a, b, c)*100
+elif a == b or a == c:
+    reward = 1000+a*100
+elif b == c:
+    reward = 1000+b*100
+print(reward)
