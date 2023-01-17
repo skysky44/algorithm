947521aebac6fabdaf8bf51684a8495251e76a50
Jeonghwan <110805149+skysky44@users.noreply.github.com>
2023-01-17 14:01:48 +0900

    [Bronze V] Title: A+B - 5, Time: 40 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/10952. A＋B － 5/A＋B － 5.py
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.py
@@ -1,4 +1,6 @@
-A, B = map(int, input().split())
-while A != 0 and B != 0:
-    print(A + B)
-    A, B = map(int, input().split())
+while True:
+    x, y = map(int, input().split())
+    if x == 0 and y == 0:
+        break
+    else:
+        print(x+y)
