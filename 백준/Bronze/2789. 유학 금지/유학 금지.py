8172e643d13c51516b656bfae5a26ba8a3c1e279
Jeonghwan <110805149+skysky44@users.noreply.github.com>
2023-01-18 13:29:03 +0900

    [Bronze II] Title: 유학 금지, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/2789. 유학 금지/유학 금지.py
+++ 백준/Bronze/2789. 유학 금지/유학 금지.py
@@ -1,7 +1,6 @@
 word = input()
-new_word = []
+new_word = ''
 for i in word:
     if i not in 'CAMBRIDGE':
-        new_word.append(i)
-for j in new_word:
-    print(j, end='')
+        new_word += i
+print(new_word)
