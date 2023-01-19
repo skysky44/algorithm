ce94defec5f4a84ffacd36c06273781792490d19
Jeonghwan <110805149+skysky44@users.noreply.github.com>
2023-01-19 15:10:40 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 4096 ms, Memory: 47664 KB -BaekjoonHub

---
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.py
@@ -0,0 +1,9 @@
+n = int(input())
+members = {}
+for _ in range(n):
+    name, state = input().split()
+    members[name] = state
+
+for member in sorted(members.keys(), reverse=True):
+    if members[member] == 'enter':
+        print(member)
