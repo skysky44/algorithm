6f5a65e54c27f675209adfadbaa7b70fa3c47536
Jeonghwan <110805149+skysky44@users.noreply.github.com>
2023-01-31 14:07:42 +0900

    [Bronze II] Title: 대표값, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2592. 대표값/대표값.py
@@ -0,0 +1,9 @@
+nums = {}
+total = 0
+for _ in range(10):
+    num = int(input())
+    total += num
+    nums[num] = nums.get(num, 0) + 1
+
+print(int(total/10))
+print(sorted(nums.items(), key=lambda x: x[1], reverse=True)[0][0])
