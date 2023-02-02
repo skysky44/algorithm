1a8413842ac14a08eab0c2713891b49f3e19b1ff
Jeonghwan <110805149+skysky44@users.noreply.github.com>
2023-02-02 18:04:39 +0900

    [Silver V] Title: 단어 나누기, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1251. 단어 나누기/단어 나누기.py
+++ 백준/Silver/1251. 단어 나누기/단어 나누기.py
@@ -3,8 +3,5 @@ result = []
 for i in range(1, len(word)-1):
     for j in range(i+1, len(word)):
         x, y, z = word[:i][::-1], word[i:j][::-1], word[j:][::-1]
-
-
         result.append(''.join(x+y+z))
-
 print(sorted(result)[0])
