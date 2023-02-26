259aa56dc75dfb97cbb668f1a64ad0005ef0ca23
Jeonghwan <110805149+skysky44@users.noreply.github.com>
2023-02-26 18:24:34 +0900

    [Bronze I] Title: 2007년, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/1924. 2007년/2007년.py
@@ -0,0 +1,9 @@
+x, y = map(int, input().split())
+day = 0
+month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+week = ["SUN", "MON", "TUE", "WED",
+        "THU", "FRI", "SAT"]
+for i in range(x-1):
+    day = day + month[i]
+day = (day + y) % 7
+print(week[day])
