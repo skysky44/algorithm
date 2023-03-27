3a22911cf2b9e7c93570621f3505ff356cf52f59
Jeonghwan <110805149+skysky44@users.noreply.github.com>
2023-03-27 20:36:27 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 4, Time: 652 ms, Memory: 70516 KB -BaekjoonHub

---
+++ 백준/Silver/24482. 알고리즘 수업 － 깊이 우선 탐색 4/알고리즘 수업 － 깊이 우선 탐색 4.py
@@ -0,0 +1,30 @@
+import sys
+sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
+N, M, R = map(int, input().split())
+
+graph = [[] for _ in range(N+1)]
+for _ in range(M):
+    u, v = map(int, input().split())
+    graph[u].append(v)
+    graph[v].append(u)
+
+for i in graph:
+    i.sort(reverse=True)
+
+visited = [-1]*(N+1)
+
+
+def dfs(v, depth):
+    visited[v] = depth
+
+    for i in graph[v]:
+        if visited[i] == -1:
+            visited[i] = depth
+            dfs(i, depth+1)
+
+
+dfs(R, 0)
+
+for i in visited[1::]:
+    print(i)
