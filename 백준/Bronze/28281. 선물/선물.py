92306fb091ffcf1cfb92a7a606517f6077b0ca78
Jeonghwan An <110805149+skysky44@users.noreply.github.com>
2023-09-13 09:54:46 +0900

    [Bronze IV] Title: 선물, Time: 92 ms, Memory: 42176 KB -BaekjoonHub

---
+++ 백준/Bronze/28281. 선물/선물.py
@@ -0,0 +1,10 @@
+import sys
+input = sys.stdin.readline
+N, X = map(int, input().split())
+socks = list(map(int, input().split()))
+price = []
+
+for i in range(N - 1):
+    price.append((socks[i]+socks[i + 1])*X)
+
+print(min(price))
