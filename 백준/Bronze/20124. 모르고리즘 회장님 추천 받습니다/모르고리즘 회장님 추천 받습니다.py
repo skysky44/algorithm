e7c4505026dad13fbcda1d9b9df910cece79093d
Jeonghwan An <110805149+skysky44@users.noreply.github.com>
2023-09-26 22:00:25 +0900

    [Bronze II] Title: 모르고리즘 회장님 추천 받습니다, Time: 220 ms, Memory: 59204 KB -BaekjoonHub

---
+++ 백준/Bronze/20124. 모르고리즘 회장님 추천 받습니다/모르고리즘 회장님 추천 받습니다.py
@@ -0,0 +1,9 @@
+import sys
+input = sys.stdin.readline
+n=int(input())
+nums=[]
+for i in range(n):
+    name, score = input().split()
+    nums.append((name,int(score)))
+nums.sort(key=lambda x: (-x[1],x[0]))
+print(nums[0][0])
