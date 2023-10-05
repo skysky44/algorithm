94698eece3085ed87ff63e7709cf9ff6604a5a13
Jeonghwan An <110805149+skysky44@users.noreply.github.com>
2023-10-06 02:32:56 +0900

    [level 2] Title: 가장 큰 수, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.py
@@ -0,0 +1,12 @@
+def solution(numbers):
+    answer = ''
+    numbers = map(str, numbers)
+    numbers = sorted(numbers, key=lambda x : x*3, reverse=True)
+
+    for number in numbers:
+        answer += number
+        
+    if int(answer) == 0:
+        answer = '0'
+        
+    return answer
