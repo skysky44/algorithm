0e6ccc1c396a07e3b0b906f4dc7f2b97ee760c24
Jeonghwan An <110805149+skysky44@users.noreply.github.com>
2023-10-05 21:31:59 +0900

    [level 2] Title: 의상, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42578. 의상/의상.py
+++ 프로그래머스/lv2/42578. 의상/의상.py
@@ -1,11 +1,10 @@
+from collections import defaultdict
+
 def solution(clothes):
-    dic_cloth = {}
+    dic_cloth = defaultdict(int)
     for cloth in clothes:
-        if cloth[1] in dic_cloth:
-            dic_cloth[cloth[1]] += 1
-        else:
-            dic_cloth[cloth[1]] = 1
-    
+        dic_cloth[cloth[1]] += 1
+        
     answer = 1
     if dic_cloth:
         for i in dic_cloth.values():
@@ -15,3 +14,20 @@ def solution(clothes):
         answer = 0
 
     return answer
+# def solution(clothes):
+#     dic_cloth = {}
+#     for cloth in clothes:
+#         if cloth[1] in dic_cloth:
+#             dic_cloth[cloth[1]] += 1
+#         else:
+#             dic_cloth[cloth[1]] = 1
+    
+#     answer = 1
+#     if dic_cloth:
+#         for i in dic_cloth.values():
+#             answer *= i+1
+#         answer -= 1
+#     else:
+#         answer = 0
+
+#     return answer
