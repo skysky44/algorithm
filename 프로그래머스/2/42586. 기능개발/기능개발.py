101d3adbb81b0b4a715b2ddff6171825d4deecee
Jeonghwan An <110805149+skysky44@users.noreply.github.com>
2023-10-29 23:05:04 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -0,0 +1,21 @@
+from collections import deque
+
+def solution(progresses, speeds):
+    progresses = deque(progresses)
+    speeds = deque(speeds)
+    answer = []
+    
+    while progresses:
+        for i in range(len(progresses)):
+            progresses[i] += speeds[i]
+        
+        cnt = 0
+        while progresses and progresses[0] >= 100:
+            progresses.popleft()
+            speeds.popleft()
+            cnt += 1
+        if cnt != 0:
+            answer.append(cnt)
+
+            
+    return answer
