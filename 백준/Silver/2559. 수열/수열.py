98f6920281f031bed9a190c151600acdb0d150b3
Jeonghwan An <110805149+skysky44@users.noreply.github.com>
2023-11-03 23:02:39 +0900

    [Silver III] Title: 수열, Time: 76 ms, Memory: 40156 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.py
+++ 백준/Silver/2559. 수열/수열.py
@@ -1,13 +1,12 @@
 import sys
 input = sys.stdin.readline
+
 N, K = map(int, input().split())
-temps = list(map(int, input().split()))
+tem = list(map(int, input().split()))
+result = []
+result.append(sum(tem[:K]))
 
-sums = [0]*(N-K+1)
+for i in range(N-K):
+    result.append(result[i] - tem[i] + tem[i+K])
 
-for i in range(K):
-    sums[0] += temps[i]
-
-for j in range(N-K):
-    sums[j+1] = sums[j]-temps[j]+temps[j+K]
-print(max(sums))
+print(max(result))
