4ac7df146bdc9d578ef0f050917519f46d0eb800
Jeonghwan An <110805149+skysky44@users.noreply.github.com>
2023-11-06 03:42:23 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 100 ms, Memory: 35316 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
@@ -1,21 +1,30 @@
 from collections import deque
-
 N, K = map(int, input().split())
-visited = [-1] * 100001
-stack = deque([N])
-visited[N] = 0
 
-while stack:
-    cur = stack.popleft()
-    if cur == K:
-        print(visited[cur])
-        break
-    for i in (cur * 2, cur - 1, cur + 1):
-        if 0 <= i <= 100000 and visited[i] == -1:
-            if i == (cur * 2):
-                visited[i] = visited[cur]
-                stack.appendleft(i)
-                
-            else:
-                visited[i] = visited[cur] + 1
-                stack.append(i)
+visited = [-1]*100001
+
+def bfs(start):
+    q = deque()
+    q.append(start)
+    visited[start] = 0
+
+    while q:
+        
+        cur = q.popleft()
+
+        if cur == K:
+            print(visited[cur])
+            return
+        
+        next = [2*cur, cur-1, cur+1]
+
+        for i in next:
+            if 0 <= i < 100001 and visited[i] == -1:
+
+                if i == 2*cur:
+                    visited[i] =  visited[cur]
+                    q.appendleft(i)
+                else:
+                    visited[i] = visited[cur] + 1
+                    q.append(i)
+bfs(N)
